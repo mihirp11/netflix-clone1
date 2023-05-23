cc019ca966bf7584f15a78276b5cfa77c09771b4
mihirp11 <114366288+mihirp11@users.noreply.github.com>
2023-05-22 20:45:03 -0400

    s

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
       "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",],
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
   theme: {
     extend: {},
   },
+  variants: {},
   plugins: [],
 }
-

db219c7ac56a603400da30f78750325b48ea2179
mihirp11 <114366288+mihirp11@users.noreply.github.com>
2023-06-16 10:02:20 -0400

    as

--- components/Billoard.tsx
+++ components/Billoard.tsx
@@ -3,37 +3,47 @@ import useBillboard from "@/hooks/useBillboard";
 import { useState } from "react";
 import { AiOutlineInfoCircle } from "react-icons/ai";
 import MuteButton from "@/components/MuteButton";
+import { GrRefresh } from "react-icons/gr";
 const Billboard = (style: any) => {
   const { data } = useBillboard();
   const [muted, setMuted] = useState(true);
+  const [ended, setEnded] = useState(false);
 
   return (
     <div style={{ ...style }} className={styles.container}>
-      <div
-        style={{
-          width: "100%",
-          height: "20%",
-          position: "absolute",
-          background: "linear-gradient(rgba(0, 0, 0, 0.9),rgba(0, 0, 0, 0.5))",
-          bottom: "15rem",
-          zIndex: "50",
-        }}
-      />
       <div className={styles.videoContainer}>
         <video
-          onClick={this?.pause()}
+          onEnded={() => {
+            setEnded(true);
+          }}
           muted={muted}
-          autoPlay
+          autoPlay={!ended}
           className={styles.video}
-          loop
           poster={data?.thumbnailUrl}
           src={data?.videoUrl}
         />
-        <MuteButton
-          style={{ position: "absolute", right: "4rem", bottom: "20%" }}
-          muted={muted}
-          setMuted={setMuted}
+        <div
+          style={{
+            width: "100%",
+            height: "20%",
+            position: "absolute",
+            bottom: "0",
+            background: "linear-gradient(rgba(0, 0, 0, 0),rgba(0, 0, 0, 0.7))",
+          }}
         />
+        {ended ? (
+          <MuteButton
+            style={{ position: "absolute", right: "4rem", bottom: "20%" }}
+            ended={ended}
+            setEnded={setEnded}
+          />
+        ) : (
+          <MuteButton
+            style={{ position: "absolute", right: "4rem", bottom: "20%" }}
+            muted={muted}
+            setMuted={setMuted}
+          />
+        )}
         <div className={styles.movieLabel}>
           <h1 className={styles.title}>{data?.title}</h1>
           <p className={styles.description}>{data?.description}</p>
@@ -45,7 +55,6 @@ const Billboard = (style: any) => {
           </button>
         </div>
       </div>
-      <div className={styles.moviesContainer}></div>
     </div>
   );
 };
--- components/MuteButton.tsx
+++ components/MuteButton.tsx
@@ -1,21 +1,34 @@
 import styles from "@/styles/mutebutton.module.css";
 import React from "react";
 import { HiOutlineSpeakerWave, HiOutlineSpeakerXMark } from "react-icons/hi2";
+import { GrRefresh } from "react-icons/gr";
 type MuteButtonProps = {
-  muted: boolean;
-  setMuted: (a: boolean) => void;
+  muted?: boolean;
+  setMuted?: (a: boolean) => void;
+  ended?: boolean;
+  setEnded?: (a: boolean) => void;
   style: any;
 };
-const MuteButton: React.FC<MuteButtonProps> = ({ muted, setMuted, style }) => {
+const MuteButton: React.FC<MuteButtonProps> = ({
+  ended,
+  setEnded,
+  muted,
+  setMuted,
+  style,
+}) => {
   return (
     <button
       className={styles.container}
       style={{
         ...style,
       }}
-      onClick={() => setMuted(!muted)}
+      onClick={() =>
+        setEnded ? setEnded(!ended) : setMuted ? setMuted(!muted) : null
+      }
     >
-      {muted ? (
+      {ended ? (
+        <GrRefresh className={styles.icon} color={"white"} />
+      ) : muted ? (
         <HiOutlineSpeakerXMark className={styles.icon} color={"white"} />
       ) : (
         <HiOutlineSpeakerWave className={styles.icon} color={"white"} />
